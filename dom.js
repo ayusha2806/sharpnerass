5248f37f2994d3a7c8245ccd39b771854ccc8ee3
ayusha2806 <anandayush2806@gmail.com>
2023-10-09 12:29:09 +0530

    change in class name and tag name

--- dom.js
+++ dom.js
@@ -41,15 +41,33 @@
 // get elements by class name
 
 
-//sharpner question 2 ans :-
+// sharpner question 2 ans :-
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[2]);
+// items[2].style.backgroundColor = 'green';
+
+//sharpner question 3 ans :-
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+
+//sharpner question  editing it with getelementsbyclassname:-
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[2]);
 items[2].style.backgroundColor = 'green';
-
-//sharpner question 3 ans :-
-
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight = 'bold';
 }
 
+//sharpner question by tag name:-
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[2]);
+li[2].style.backgroundColor = 'green';
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight = 'bold';
+}
