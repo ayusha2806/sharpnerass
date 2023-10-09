db78eeb91541128e00b37c871fd959c6050009ac
ayusha2806 <anandayush2806@gmail.com>
2023-10-09 13:18:15 +0530

    commit by queryselector/all

--- dom.js
+++ dom.js
@@ -55,19 +55,36 @@
 
 
 //sharpner question  editing it with getelementsbyclassname:-
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor = 'green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
-}
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[2]);
+// items[2].style.backgroundColor = 'green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
 
-//sharpner question by tag name:-
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[2]);
-li[2].style.backgroundColor = 'green';
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight = 'bold';
+// //sharpner question by tag name:-
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[2]);
+// li[2].style.backgroundColor = 'green';
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight = 'bold';
+// }
+
+// queryselector sharpner question:-
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'black';
+//queryselector sharpner question:-
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.color = 'transparent';
+
+//queryselectorALL sharpner question:-
+var items = document.querySelectorAll('.list-group-item');
+console.log(items);
+items[1].style.color='green';
+//queryselectorALL sharpner question:-
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for (var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor="green";
 }
